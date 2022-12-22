775951f04f7379a63b9cb447d55dbdcd477b57f3
Og'abek <amirdev25@gmail.com>
2022-12-22 10:42:47 +0500

    Firts uplod: Date: 22.12.2022

---
+++ android/app/src/main/kotlin/com/example/intrinsic_height/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.intrinsic_height
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "IntrinsicHeight",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            child: Text(
+              "IntrinsicHeight",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20.0,
+                fontFamily: FontStyle.italic.name,
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Column(
+                children: [
+                  Image.network(
+                      "https://images.unsplash.com/photo-1671540675967-7342577470d0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80"),
+                  MyTitle("Text1")
+                ],
+              ),
+              Column(
+                children: [
+                  Image.network(
+                      "https://images.unsplash.com/photo-1671540675967-7342577470d0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80"),
+                  MyTitle("Text1")
+                ],
+              ),
+              Column(
+                children: [
+                  Image.network(
+                      "https://images.unsplash.com/photo-1671540675967-7342577470d0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80"),
+                  MyTitle("Text1")
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MyTitle extends StatelessWidget {
+  String title;
+
+  MyTitle(this.title, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: const TextStyle(
+        color: Colors.black,
+        fontSize: 16.0,
+      ),
+    );
+  }
+}
